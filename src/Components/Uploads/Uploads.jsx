8b96ecd9f37c1fe4f5defa740e1f3418f3a32189
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-07-25 16:13:38 +0530

    Upload Option Updated

--- src/Components/Uploads/Uploads.jsx
+++ src/Components/Uploads/Uploads.jsx
@@ -4,6 +4,9 @@ import { Sidebar } from '../Sidebar/Sidebar'
 import "../Home/Home.scss"
 import "../RightSideStories/RightSideStories.scss"
 import "./Uploads.scss"
+import { BsCollectionPlayFill } from "react-icons/bs";
+import { FaPhotoVideo } from "react-icons/fa";
+import { GiVintageRobot } from "react-icons/gi";
 
 const Uploads = () => {
   return (
@@ -18,19 +21,19 @@ const Uploads = () => {
 
                  <div id="card1" class="card" style={{"--i":"-1"}}>
                     <div class="content" style={{"--j":"1"}}>
-                        <h2>Upload Photo</h2>
+                        <h2><FaPhotoVideo /></h2>
                     </div>            
                 </div>
 
                <div id="card2" class="card" style={{"--i":"0"}}>
                     <div class="content" style={{"--j":"2"}}>
-                        <h2>Upload Video</h2>
+                        <h2><BsCollectionPlayFill /></h2>
                     </div>            
                 </div>
 
                 <div id="card3" class="card" style={{"--i":"1"}}>
                     <div class="content" style={{"--j":"3"}}>
-                        <h2>Upload Story</h2>
+                        <h2><GiVintageRobot /></h2>
                     </div>            
                 </div>
 
